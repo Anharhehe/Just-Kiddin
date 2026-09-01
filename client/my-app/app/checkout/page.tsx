d3c361fc18cc9f580260e46853304cfc9b625e21
Anharhehe <muhammadanharmunir@gmail.com>
2026-09-01 09:27:20 +0500

    Project Update no 2

--- client/my-app/app/checkout/page.tsx
+++ client/my-app/app/checkout/page.tsx
@@ -1,5 +1,7 @@
 ﻿"use client";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5000";
+
 import { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
